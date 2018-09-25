412e664a0b07e039944db092453d426ededd700b
ankit-patel10 <ankitp5469@gmail.com>
2018-09-25 12:20:01 -0400

    Updated readme

--- extension.js
+++ extension.js
@@ -1,22 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const george = require('./george.js');
 const launch = require('./.vscode/launch.json');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gerogecode" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.george', function () {
-        // The code you place here will be executed every time your command is executed
         var x = vscode.window.activeTextEditor.document.getText()
         george(x, callback => {
             vscode.window.showInformationMessage(callback);
@@ -27,7 +15,6 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
